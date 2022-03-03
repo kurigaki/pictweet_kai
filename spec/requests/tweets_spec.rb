425d3a2ed4a88b240deb93ce25e28a7779fb43bd
kurigaki <kenji.kurigaki@gmail.com>
2022-03-03 18:39:08 +0900

    テストコードの記述

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -25,12 +25,20 @@ describe TweetsController, type: :request do
   end
   describe 'GET #show' do
     it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get tweet_path(@tweet)
+      expect(response.status).to eq 200
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include(@tweet.text)
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include(@tweet.image)
     end
     it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include('＜コメント一覧＞')
     end
   end 
 end
