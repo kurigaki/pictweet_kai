c8a1c107b21b27c826222ea05293e3486d14e972
kurigaki <kenji.kurigaki@gmail.com>
2022-03-07 14:52:55 +0900

    テストコードの編集

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -21,13 +21,13 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       expect{
         find('input[name="commit"]').click
       }.to change { User.count }.by(1)
-      # トップページへ遷移したことを確認する
+      # トップページへ遷移することを確認する
       expect(current_path).to eq(root_path)
       # カーソルを合わせるとログアウトボタンが表示されることを確認する
       expect(
         find('.user_nav').find('span').hover
       ).to have_content('ログアウト')
-      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
       expect(page).to have_no_content('新規登録')
       expect(page).to have_no_content('ログイン')
     end
