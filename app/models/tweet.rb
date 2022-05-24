1cd30a4115dd7a49263ee30e01d5ef3ea5ddd26d
kurigaki <kenji.kurigaki@gmail.com>
2022-05-24 23:22:05 +0900

    SQLインジェクションの対策

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,8 @@ class Tweet < ApplicationRecord
 
   def self.search(search)
     if search != ""
-      Tweet.where('text LIKE(?)', "%#{search}%")
+       search = "%#{search}%"
+       Tweet.find_by_sql(["select * from tweets where text like ? ", search])
     else
       Tweet.all
     end
